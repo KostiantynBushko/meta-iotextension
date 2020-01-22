ca0daa6279ec33530a8bf3199f42df3eb08f19d4
Kostiantyn Bushko <kostiantyn.bushko@gmail.com>
2020-01-22 22:20:58 +0200

    set hostname to cpuid
    Signed-off-by: Kostiantyn Bushko <kostiantyn.bushko@gmail.com>

--- recipes-extended/runtime-config/files/hostname2cpuid.sh
+++ recipes-extended/runtime-config/files/hostname2cpuid.sh
@@ -3,4 +3,12 @@
 CPUID=''
 CPUID=$(echo $CPUID | cat /proc/cpuinfo | grep Serial | cut -d ' ' -f 2 | tr '[:lower:]' '[:upper:]')
 
-echo "hostname to cpuid: ${CPUID}"
+if [ -z ${CPUID} ]; then
+  echo "ERROR: Impossible obtain CPUID"
+  exit 1
+fi
+
+echo ${CPUID} > /etc/hostname 
+cat /etc/hostname
+
+exit 0

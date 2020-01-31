f76f6a075d97ad4f322a93c3fde0222f9bc2f962
Kostiantyn Bushko <kostiantyn.bushko@gmail.com>
2020-01-31 02:52:20 +0200

    check if hostname already configured + restart zabbix agent
    Signed-off-by: Kostiantyn Bushko <kostiantyn.bushko@gmail.com>

--- recipes-extended/runtime-config/files/zabbix-hostname.sh
+++ recipes-extended/runtime-config/files/zabbix-hostname.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-./$1 | while read -r line; do
+./$1 | while read -r line; do 
   if [[ ${line} == *"Device ID: "* ]]; then
-    sed -i "s:Hostname=Zabbix server:Hostname=${line:11}:g" /etc/zabbix_agentd.conf 
+    grep -Fxq "^Hostname=${line:11}" /etc/zabbix_agent.conf 
+    if [[ $? -eq 1 ]]; then 
+      sed -i "s:^Hostname=Zabbix server:Hostname=${line:11}:g" /etc/zabbix_agentd.conf 
+      systemctl restart zabbix-agent.service
+    fi 
     exit 0
   fi
 done

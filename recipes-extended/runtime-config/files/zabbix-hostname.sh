0d0bb1c180d15a977e4ac078537eec967dad6a6a
Kostiantyn Bushko <kostiantyn.bushko@gmail.com>
2020-02-03 13:56:50 +0200

    use temporary plain text file for parsing aktualizr info
    Signed-off-by: Kostiantyn Bushko <kostiantyn.bushko@gmail.com>

--- recipes-extended/runtime-config/files/zabbix-hostname.sh
+++ recipes-extended/runtime-config/files/zabbix-hostname.sh
@@ -1,31 +1,22 @@
 #!/bin/sh
 
 S=1
-$1 | while read -r line; do
-  if [[ "${line:6}" == "Error:" ]]; then
-    exit 1
-  fi
+$1 > /tmp/aktualizr-info-tmp
+while read -r line; do
   if [[ "${line:0:10}" == "Device ID:" ]]; then
     grep -Fxq "Hostname=${line:11}" /etc/zabbix_agentd.conf 
     if [[ $? -eq 1 ]]; then 
       sed -i "s:^Hostname=Zabbix server:Hostname=${line:11}:g" /etc/zabbix_agentd.conf 
       systemctl restart zabbix-agent.service
-      echo " ** SET HOSTNAME: ${line:11}" 
-      let S=0
-      #echo $S
-      #break
-    else
-      echo " ** Host Nmae was already SET"
-      let S=0
-      #echo $S
-      #break
     fi 
+    S=0
   fi
-done
+done < "/tmp/aktualizr-info-tmp"
 
-echo "STATUS ${S}"
+rm /tmp/aktualizr-info-tmp
+
 if [[ "${S}" -ne 0 ]]; then
- echo " ** FAILED nead restart service"
+ echo "ERROR: Reading aktualizr info failed. Restart service!"
  sleep 20
  systemctl restart zabbix-hostname.service
  exit 1 
